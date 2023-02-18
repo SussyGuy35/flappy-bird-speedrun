5459b648f0f56a1f16067782fe1198b799b83a1d
BachNob <tqbach184@gmail.com>
2023-02-18 21:40:04 +0700

    at least can jump bird

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame, sys
 
 class Bird(pygame.sprite.Sprite):
-    def __init__(self,width,height,pos,color):
+    def __init__(self,width,height,pos,color,grv,jump_force):
         super().__init__()
         self.rect = pygame.rect.Rect(0,0,width,height)
         self.color = color
         self.rect.center = pos
+        self.vsp = 0
+        self.grv = grv
+        self.jump_force = jump_force
     def update(self):
-        pass
+        self.vsp += self.grv
+        self.rect.y += self.vsp
     def draw(self):
         pygame.draw.ellipse(screen,self.color,self.rect)
 
@@ -23,16 +27,21 @@ clock = pygame.time.Clock()
 pygame.display.set_caption(title)
 
 # Sth
-bird = Bird(32,32,(screen_width/4,screen_height/2),(255,255,255))
+bird = Bird(32,32,(screen_width/4,screen_height/2),(255,255,255),0.3,7)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            bird.vsp = -bird.jump_force
+
 
     screen.fill((64,128,64))
 
+    bird.update()
     bird.draw()
 
     pygame.display.flip()
