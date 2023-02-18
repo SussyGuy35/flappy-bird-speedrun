981e0082cd845c53b70ddc96db22b97bebe1bcbb
BachNob <tqbach184@gmail.com>
2023-02-18 21:35:36 +0700

    add bird but not working yet

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame, sys
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self,width,height,pos,color):
+        super().__init__()
+        self.rect = pygame.rect.Rect(0,0,width,height)
+        self.color = color
+        self.rect.center = pos
+    def update(self):
+        pass
+    def draw(self):
+        pygame.draw.ellipse(screen,self.color,self.rect)
 
 
 # Config
@@ -12,13 +22,18 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 pygame.display.set_caption(title)
 
+# Sth
+bird = Bird(32,32,(screen_width/4,screen_height/2),(255,255,255))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    screen.fill((64,128,64))
 
+    bird.draw()
 
     pygame.display.flip()
     clock.tick(game_speed)        
